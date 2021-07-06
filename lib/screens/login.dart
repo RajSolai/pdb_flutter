4a7b58cfd7d044c613f792a996f349ae0a7a00d1
Solai Raj <msraj085@gmail.com>
2021-07-06 13:08:35 +0530

    added login code

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key? key}) : super(key: key);
@@ -8,6 +10,18 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String _loginCode = "";
+
+  void getToken() async {
+    var pref = await SharedPreferences.getInstance();
+    var data = {"passcode": _loginCode};
+    var res = await Dio().post("", data: data);
+    print(res.statusMessage);
+    if (res.statusMessage != "Invalid Passcode") {
+      pref.setString("token", res.data['token']);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +32,9 @@ class _LoginScreenState extends State<LoginScreen> {
               Center(
                 child: Container(
                   child: TextField(
+                    onChanged: (val) => setState(() {
+                      _loginCode = val;
+                    }),
                     decoration: InputDecoration(
                       suffixIcon: Icon(Icons.login_rounded),
                       hintText: "Enter Login Code",
